2aeaae6921e2841e170a54deaee00132b8273413
ittikhon <77088676+ittikhon@users.noreply.github.com>
2021-02-11 16:59:18 +0700

    Update Card.java

--- lect04/Card.java
+++ lect04/Card.java
@@ -1,58 +1,60 @@
 public class Card {
-  public static final String[] RANKS = { 
-    null, "Ace", "2", "3", "4", "5", "6",
-    "7", "8", "9", "10", "Jack", "Queen", "King"};
 
-  public static final String[] SUITS = {
-    "Clubs", "Diamonds", "Hearts", "Spades"};
+public enum Rank {
+  ACE,
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JACK,
+  QUEEN,
+  KING
+}
+
+public enum Suit {
+  CLUBS,
+  DIAMOND,
+  HEARTS,
+  SPADES
+}
 
   private final int rank;
   private final int suit;
-  
-  // Queens of Spades: rank = 12, suit=3
-  // Five of Diamonds: rank = 5, suit=1
 
-  public Card(int rank, int suit) {
+
+  public Card(RANK rank, Suit suit) {
     this.rank = rank;
     this.suit = suit;
   }
-
-  //override method toString of java.lang.Object
   public String toString() {
-    String s = RANKS[this.rank] + " of " + SUITS[this.suit];
+    String s = this.rank + " of " + this.suit;
     return s;
   }
-
   public boolean equals(Card that) {
     return this.rank == that.rank && this.suit == that.suit;
   }
-
-  // compareTo 
   public int compareTo(Card that) {
-    if (this.suit < that.suit) {
-      return -1;
+    int cmp_suit = this.suit.compareTo(that.suit);
+    int cmp_rank = this.rank.compareTo(that.rank);
+
+    if ((cmp_suit == 0) && (cmp_rank == 0)) {
+      return 0;
     }
-    if (this.suit > that.suit) {
-      return 1;
+    if (cmp_suit != 0) {
+      return cmp_suit;
+    } else {
+      return cmp_rank;
     }
-    if (this.rank < that.rank) {
-      return -1;
-    }
-    if (this.rank > that.rank) {
-      return 1;
-    }
-    return 0;
   }
-  // accessor method
   public int getRank() {
     return this.rank;
   }
-
-  // accessor method
   public int getSuit() {
     return this.suit;
   }
-
-  // no setter method due to 'immutable' 
-
 }
